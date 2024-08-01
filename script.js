efde5ae0d3067fda7dd83fc1d8777111a18b1207
carahuor <carahuor@gmail.com>
2024-08-01 20:25:01 +0000

    feat: add Earth screen and ending screen

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+/* VARIABLES */
 //Global Variables
 let screen = 0;
 
@@ -6,7 +7,20 @@ let screen = 0;
 let button;
 let bPlay;
 
+//Variables for Intro Dialogue
+let name;
+let sayCount=0;
+let choice1;
+let choice2;
+let speech = ["Hi there, astronaut! I'm Sky,\na flight controller of Mission: Mars.\nI'll be helping you throught the mission.", 
+"\nYou're the first person to set foot on Mars! \nIsn't that exciting?",
+"Now, before you set off, you're going to \nneed to prepare your spaceship for lift off. \nI'll be leading you through the communication \nsystem.", 
+"\nUh oh, looks like the spaceship is malfunctioning.",
+"\nYou're going to be alright! Have hope! \nLet- see wh- w- can d-",
+"You'll need to navigate back to Earth yourself! \nMove your mouse up and down to steer your \nspaceship away from asteroids and return to \nEarth! *silence*"];
+
 //Variables for Asteroid Game
+let started = false;
 let count = 0;
 let rock;
 let s;
@@ -16,7 +30,17 @@ let r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, 
 
 let ship;
 let wall;
+//let earth;
 
+//Variable for end screen
+let bAgain;
+
+/* PRELOAD LOADS FILES */
+function preload(){
+
+}
+
+/* SETUP RUNS ONCE */
 function setup() {
   createCanvas(400, 400);
   background(40);
@@ -30,6 +54,9 @@ function setup() {
   bPlay = new button.Sprite(-100, -100);
 
 
+  //Variables from Dialogue Screen
+  choice1 = new button.Sprite(-100, -100);
+  choice2 = new button.Sprite(-100, -100);
 
   //Variables for Asteroid Game
 
@@ -44,7 +71,7 @@ function setup() {
   new wall.Sprite(height,-1,1,canvas.height);
 
   //Value for spacing between rocks
-  s = 10;
+  s = 20;
 
   //Rock Group
   rock = new Group();
@@ -61,119 +88,200 @@ function setup() {
   w = width/2;
 
   //1st Column of Rocks
-  r1 = new rock.Sprite();
-  r2 = new rock.Sprite();
-  r3 = new rock.Sprite();
-  r4 = new rock.Sprite();
+  r1 = new rock.Sprite(-100,-100);
+  r2 = new rock.Sprite(-100,-100);
+  r3 = new rock.Sprite(-100,-100);
+  r4 = new rock.Sprite(-100,-100);
 
   //2nd Column of Rocks
-  r5 = new rock.Sprite();
-  r6 = new rock.Sprite();
-  r7 = new rock.Sprite();
+  r5 = new rock.Sprite(-100,-100);
+  r6 = new rock.Sprite(-100,-100);
+  r7 = new rock.Sprite(-100,-100);
 
   //3rd Column of Rocks
-  r8 = new rock.Sprite();
-  r9 = new rock.Sprite();
-  r10 = new rock.Sprite();
+  r8 = new rock.Sprite(-100,-100);
+  r9 = new rock.Sprite(-100,-100);
+  r10 = new rock.Sprite(-100,-100);
 
   //4th Column of Rocks
-  r11 = new rock.Sprite();
-  r12 = new rock.Sprite();
-  r13 = new rock.Sprite();
+  r11 = new rock.Sprite(-100,-100);
+  r12 = new rock.Sprite(-100,-100);
+  r13 = new rock.Sprite(-100,-100);
 
   //5th Column of Rocks
-  r14 = new rock.Sprite();
-  r15 = new rock.Sprite();
-  r16 = new rock.Sprite();
-  r17 = new rock.Sprite();
+  r14 = new rock.Sprite(-100,-100);
+  r15 = new rock.Sprite(-100,-100);
+  r16 = new rock.Sprite(-100,-100);
+  r17 = new rock.Sprite(-100,-100);
 
   //6th Column of Rocks
-  r18 = new rock.Sprite();
-  r19 = new rock.Sprite();
-  r20 = new rock.Sprite();
-  r21 = new rock.Sprite();
+  r18 = new rock.Sprite(-100,-100);
+  r19 = new rock.Sprite(-100,-100);
+  r20 = new rock.Sprite(-100,-100);
+  r21 = new rock.Sprite(-100,-100);
 
-  restore();
+  ship = new Sprite(-100,-100);
+  ship.d = 50;
 
-  ship = new Sprite(15, h3, 50);
+  //earth = new Sprite(-200,200, 50, "k");
+  //earth.color = new color(255);
+
+  //End Screen
+  bAgain = new button.Sprite(-100,-100);
 }
 
+/* DRAW LOOP REPEATS */
 function draw() {
   noStroke();
-  //showHomeScreen();
-  //if (screen == 1){
+  if (screen == 0){
+      showHomeScreen();
+  } else if (screen == 1){
+    showDialogueScreen();
+  } else if (screen == 2){
+    if (!started){
+      restore();
+      ship.pos ={x:15, y:h3};
+      started = true;
+    }
     showAsteroidScreen();
-  //}
+  }
+  else if (screen == 3){
+    showEarthScreen();
+  } else if (screen == 4){
+    showEndScreen();
+  }
 
 
 }
 
+/* FUNCTIONS */
 function showHomeScreen(){
   background(40);
   fill(255);
-  bPlay.pos ={x:width/2, y:height/2+100};
-  if (mouse.presses(bPlay)){
-    screen = 1;
+  bPlay.pos = {x:width/2, y:height/2+100};
+  bPlay.text = "Play";
+  if (bPlay.mouse.presses()){
+    bPlay.pos = {x:-100,y:100};
+    screen++;
   }
+}
+
+function showDialogueScreen(){
+  textAlign(CENTER);
+  textSize(15);
+
+  background(40);
+  fill(255);
+  rect(20,20,width-40,100,10,10,10,10);
+  text("Press 'space' to continue", width/2, 140);
+  fill(0);
+  text(speech[sayCount], width/2, 50);
+
+
+  if (kb.pressed("space")){
+    sayCount++;
+  } 
+  if (sayCount == 6){
+    screen++;
+    sayCount = 0;
+  }
+
+
+
 
 }
 
 function showAsteroidScreen(){
   background(40);
   fill(255);
-  text("Restart Counter: "+count,10, 15);
-
-  //restore();
-
-  //Rocks travel left across the screen until last column (save CPU resources)
-
-  if (r21.x == -1*rock.r){
-    rock.vel.x = 0;
-  } else {
-    rock.vel.x = -4;
-  }
+  textAlign(LEFT);
+  textSize(10);
+  text("Move your mouse up and down to steer your spaceship away from asteroids\nRestart Counter: "+count,10, 15);
 
   //Move ship with mouse
-  ship.moveTo(mouse, 15);
+  ship.moveTowards(35, mouse.y, 1);
 
+  //Resets to beginning when ship hits a rock
   if (ship.collides(rock)){
     restore();
     count++;
+  }
+
+  //Rocks travel left across the screen until last column (save CPU resources)
+  if (r21.x == 0){
+    rock.vel.x = 0;
+    screen++;
+    count = 0;
+    started = false;
+  } else {
+    rock.vel.x = -4;
   }
 
 }
 
 function restore(){
   //1st Column of Rocks
-  r1.pos = {x:w,y:h1};
-  r2.pos = {x:w,y:h2};
-  r3.pos = {x:w,y:h4};
-  r4.pos = {x:w,y:h5};
+  r1.pos = {x:w+100,y:h1};
+  r2.pos = {x:w+100,y:h2};
+  r3.pos = {x:w+100,y:h4};
+  r4.pos = {x:w+100,y:h5};
 
   //2nd Column of Rocks
-  r5.pos = {x:2*w,y:h1};
-  r6.pos = {x:2*w,y:h3};
-  r7.pos = {x:2*w,y:h5};
+  r5.pos = {x:2*w+100,y:h1};
+  r6.pos = {x:2*w+100,y:h3};
+  r7.pos = {x:2*w+100,y:h5};
 
   //3rd Column of Rocks
-  r8.pos = {x:3*w,y:h2};
-  r9.pos = {x:3*w,y:h3};
-  r10.pos = {x:3*w,y:h4};
+  r8.pos = {x:3*w+100,y:h2};
+  r9.pos = {x:3*w+100,y:h3};
+  r10.pos = {x:3*w+100,y:h4};
 
   //4th Column of Rocks
-  r11.pos = {x:4*w,y:h1};
-  r12.pos = {x:4*w,y:h2};
-  r13.pos = {x:4*w,y:h3};
+  r11.pos = {x:4*w+100,y:h1};
+  r12.pos = {x:4*w+100,y:h2};
+  r13.pos = {x:4*w+100,y:h3};
 
   //5th Column of Rocks
-  r14.pos = {x:5*w,y:h1};
-  r15.pos = {x:5*w,y:h3};
-  r16.pos = {x:5*w,y:h4};
-  r17.pos = {x:5*w,y:h5};
+  r14.pos = {x:5*w+100,y:h1};
+  r15.pos = {x:5*w+100,y:h3};
+  r16.pos = {x:5*w+100,y:h4};
+  r17.pos = {x:5*w+100,y:h5};
 
   //6th Column of Rocks
-  r18.pos = {x:6*w,y:h1};
-  r19.pos = {x:6*w,y:h2}; 
-  r20.pos = {x:6*w,y:h4}; 
-  r21.pos = {x:6*w,y:h5}; 
+  r18.pos = {x:6*w+100,y:h1};
+  r19.pos = {x:6*w+100,y:h2}; 
+  r20.pos = {x:6*w+100,y:h4}; 
+  r21.pos = {x:6*w+100,y:h5}; 
+
+  //earth.pos = {x:9*w+100,y:h3};
+}
+
+function showEarthScreen(){
+  background(40);
+  fill(255);
+  textAlign(CENTER);
+  textSize(20);
+  text("Click Earth to return home", width/2, height/2-100);
+  if (mouseIsPressed){
+    ship.pos = {x:-100,y:-100};
+    r18.pos = {x:-100,y:-100};
+    r19.pos = {x:-100,y:-100};
+    r20.pos = {x:-100,y:-100};
+    r21.pos = {x:-100,y:-100};
+    screen++;
+  }
+}
+
+function showEndScreen(){
+  background(40);
+  bAgain.pos = {x:width/2, y:height/2+100};
+  bAgain.text = "Play Again";
+  fill(255);
+  textAlign(CENTER);
+  textSize(20);
+  text("You did it!", width/2, 100);
+  if (bAgain.mouse.presses()){
+    bAgain.pos = {x:-100,y:-100};
+    screen=0;
+  }
 }
